3e4959fc0dc60554d697e85ec5b070b49e2be70d
JaredBenitez <Jaredmbenitez@gmail.com>
2022-07-25 17:16:44 -0700

    current-use api route

---
+++ pages/api/current-user.js
@@ -0,0 +1,18 @@
+import jwt_decode from 'jwt-decode';
+
+export default async function handler(req,res){
+// return the current user making the request based off the jwt in the header
+// if the user is not logged in, return null
+// if the user is logged in, return the user object
+// Get jwt from cookie
+const token = req.cookies.jwt;
+    if (token) {
+    const decoded= jwt_decode(token);
+    res.send(decoded);
+    }
+    else{
+        res.send(null);
+    }
+
+
+}
