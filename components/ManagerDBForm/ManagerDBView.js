2e25bd478ca89b5b9d6009f76a1c979d0bef497b
JaredBenitez <Jaredmbenitez@gmail.com>
2022-07-28 11:18:30 -0700

    Added sort by name on results

--- components/ManagerDBForm/ManagerDBView.js
+++ components/ManagerDBForm/ManagerDBView.js
@@ -79,7 +79,8 @@ const renderResults = (results) =>{
           </tr>
         </thead>
         <tbody>
-          {results.map(result => {
+          {/* sort by name */}
+          {results.sort((a, b) => (a.emp_name > b.emp_name) ? 1 : -1).map(result => {
             return (
               <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
                 <td className="text-md text-gray-900 font-light px-6 py-4 whitespace-nowrap text-left">{result.emp_name}</td>
